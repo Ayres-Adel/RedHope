1e2766e7091d3d30845bf6b6d1a5d562b8223f72
Abdennour Kellal <117914376+Abdoukl@users.noreply.github.com>
2025-06-02 14:17:57 +0100

    lol

--- my-react-app/src/config.js
+++ my-react-app/src/config.js
@@ -1,7 +1,7 @@
 /*Global configuration for the application*/
 
 // Base URL for the API
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'https://redhope-backend.onrender.com';
 
 // API key for geocoding (OpenCage)
 export const GEOCODE_API_KEY = '4bcabb4ac4e54f1692c1e4d811bb29e5';
